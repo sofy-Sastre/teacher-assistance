0b675640c8eb8939867cfd490bd224d775548e12
Candela Richter <richtercandela@gmail.com>
2022-08-02 20:16:01 -0300

    Solucion error

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Fragment } from 'react';
+import Login from './UI/components/Login';
+function App() {
+  return (
+    <Fragment>
+      <Login></Login>
+    </Fragment>
+  );
+}
+
+export default App;
