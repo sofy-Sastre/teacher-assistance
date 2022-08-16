3358f142ca8385af07f509a633f2dac0ccaa1af2
agustina0203 <agustinaespinosa2011@gmail.com>
2022-08-16 20:01:10 -0300

    pantalla1

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import './App.css';
 import { Fragment } from 'react';
 import Login from './UI/components/Login';
@@ -6,7 +7,26 @@ function App() {
     <Fragment>
       <Login></Login>
     </Fragment>
+=======
+import { Fragment} from "react";
+import React from "react";
+import Pantalla1 from './UI/components/Pantalla1';
+
+
+export default function App() {
+
+  return (
+    <Fragment>
+      <Pantalla1></Pantalla1>
+      
+    </Fragment>  
+>>>>>>> Stashed changes
   );
 }
 
-export default App;
+
+
+
+
+    
+
