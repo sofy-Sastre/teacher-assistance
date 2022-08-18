9c001ea22435d8777558ab8a7de99100dfe9625e
agustina0203 <agustinaespinosa2011@gmail.com>
2022-08-17 22:09:22 -0300

    cambio

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import './App.css';
 import { Fragment } from 'react';
 // import Login from './UI/components/Login';
 import Pantallapincipal from './UI/components/Pantallaprincipal';
+import Cursos from './UI/components/Cursos';
+import Input_cursos from './UI/components/Input_cursos';
 
 export default function App() {
   return (
     <Fragment>
       {/* <Login></Login> */}
       <Pantallapincipal></Pantallapincipal>
+      <Cursos></Cursos>
+      <Input_cursos></Input_cursos>
     </Fragment>
   );    
 }
