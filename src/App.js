b1e0433dacbc890f79fa58483217ac12c21e4c25
agustina0203 <agustinaespinosa2011@gmail.com>
2022-08-17 22:14:22 -0300

    eliminacion de comentarios

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 
 import { Fragment } from 'react';
-// import Login from './UI/components/Login';
+import Login from './UI/components/Login';
 import Pantallapincipal from './UI/components/Pantallaprincipal';
 import Cursos from './UI/components/Cursos';
 import Input_cursos from './UI/components/Input_cursos';
@@ -10,7 +10,7 @@ import Input_cursos from './UI/components/Input_cursos';
 export default function App() {
   return (
     <Fragment>
-      {/* <Login></Login> */}
+      <Login></Login> 
       <Pantallapincipal></Pantallapincipal>
       <Cursos></Cursos>
       <Input_cursos></Input_cursos>
