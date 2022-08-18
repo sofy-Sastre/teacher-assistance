1cd822e98c2d669e90914d9f7b0e62231472c6a6
Sofy <estofatuby@gmail.com>
2022-08-18 14:27:44 -0300

    app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 
 import './App.css';
-
 import { Fragment } from 'react';
 import Login from './UI/components/Login';
-<<<<<<< HEAD
 import Registro from './UI/components/Registro';
-function App() {
-  return (
-    <Fragment>
-     <Registro></Registro>
-     {/* <Login></Login> */}
-=======
 import Pantallapincipal from './UI/components/Pantallaprincipal';
 import Cursos from './UI/components/Cursos';
-import Input_cursos from './UI/components/Input_cursos';
+import Input_cursos from './UI/components/Input_cursos'; 
 
 export default function App() {
   return (
     <Fragment>
       <Login></Login> 
+      <Registro></Registro>
       <Pantallapincipal></Pantallapincipal>
       <Cursos></Cursos>
       <Input_cursos></Input_cursos>
->>>>>>> b1e0433dacbc890f79fa58483217ac12c21e4c25
     </Fragment>
   );    
 }
