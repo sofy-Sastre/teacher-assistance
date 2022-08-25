7efe071f4c2f6603075227f8a2eaaace4de141f3
Sofy <estofatuby@gmail.com>
2022-08-24 23:32:31 -0300

    cambios

--- src/UI/components/Boton.jsx
+++ src/UI/components/Boton.jsx
@@ -1,29 +1,15 @@
-<<<<<<< HEAD
-// import { Fragment } from "react"
-// import { Row, Col} from "react-bootstrap";
-// import "./styles/Boton.css";
-// export default function Input(props) {
-//     return (
-//         <Fragment>
-//             <Row>
-//             <button className="boton">Iniciar sesion</button><br />
-//             </Row>
-//         </Fragment>
 
-//     )
-// }
-=======
 import { Fragment } from "react"
 import { Row, Col} from "react-bootstrap";
 import "./styles/Boton.css";
 export default function Input(props) {
-     return (
-         <Fragment>
-             <Row>
-             <button className="boton">Iniciar sesion</button><br />
-             </Row>
-         </Fragment>
+    return (
+        <Fragment>
+            <Row>
+            <button className="boton">Iniciar sesion</button><br />
+            </Row>
+        </Fragment>
 
-      )
-  }
->>>>>>> b1e0433dacbc890f79fa58483217ac12c21e4c25
+    )
+}
+
--- src/UI/components/Input.jsx
+++ src/UI/components/Input.jsx
@@ -1,39 +1,19 @@
-<<<<<<< HEAD
-// import { Fragment } from "react";
-// import "./styles/Input.css";
-// import { Row, Col} from "react-bootstrap";
 
-// export default function Input(props) {
-//     return (
-//         <Fragment>
-//             <Row className="contenedor">
-//             <Row className="label">
-//             <label htmlFor="numero">{props.nombre}</label>
-//             </Row>
-//             <Row>
-//             <input className="input" type="text" id="numero" name="numero" placeholder={props.placeh} />
-//             </Row>
-//             </Row>
-//         </Fragment>
-//     )
-// }
-=======
- import { Fragment } from "react";
- import "./styles/Input.css";
- import { Row, Col} from "react-bootstrap";
+import { Fragment } from "react";
+import "./styles/Input.css";
+import { Row, Col} from "react-bootstrap";
 
- export default function Input(props) {
-     return (
-         <Fragment>
-             <Row className="contenedor">
-             <Row className="label">
-             <label htmlFor="numero">{props.nombre}</label>
-             </Row>
-             <Row>
-             <input className="input" type="text" id="numero" name="numero" placeholder={props.placeh} />
-             </Row>
-             </Row>
-         </Fragment>
-     )
- }
->>>>>>> b1e0433dacbc890f79fa58483217ac12c21e4c25
+export default function Input(props) {
+    return (
+        <Fragment>
+            <Row className="contenedor">
+            <Row className="label">
+            <label htmlFor="numero">{props.nombre}</label>
+            </Row>
+            <Row>
+            <input className="input" type="text" id="numero" name="numero" placeholder={props.placeh} />
+            </Row>
+            </Row>
+        </Fragment>
+    )
+}
