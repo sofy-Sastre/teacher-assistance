a8e534a60a4fa11e4a8e6b607ad78f33983f0e35
Sofy <estofatuby@gmail.com>
2022-08-31 00:27:22 -0300

    cambios

--- src/UI/components/Input_props.jsx
+++ src/UI/components/Input_props.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-
+import "./styles/Registro.css"
 
 export default function Input_props(props) {
     return (
         <div>
-            <label htmlFor="numero" className="titulo">{props.nombre}</label><br />
+            <label htmlFor="numero" >{props.nombre}</label><br />
             <input className="input" type="text" id="numero" name="numero" placeholder={props.place} />
         </div>
     )
--- src/UI/components/Registro.jsx
+++ src/UI/components/Registro.jsx
@@ -11,30 +11,33 @@ export default function Registro() {
                 <div className="titulo">
                     <h1>Registro</h1>
                     <p>Complete los siguientes datos</p>
+                    <form >
+                        <div className="input-props">
+                        <div>
+                            <Input_props nombre="Nombre Completo" place="Nombre y Apellido"></Input_props>
+                        </div>
+
+                        <div>
+                            <Input_props nombre="N° Empleado" place="*****"></Input_props>
+                        </div>
+
+                        <div>
+                            <Input_props nombre="E-mail" place="Correo@correo.com"></Input_props>
+                        </div>
+
+                        <div>
+                            <Input_props nombre="Contraseña" place="Introduzca su contraseña"></Input_props>
+                        </div>
+                        </div>
+
+
+                        <div className="boton-cuenta">
+                            <button>Crear Cuenta</button><br />
+                            <p><a>¿Ya tienes cuenta? Iniciar sesion</a></p>
+                        </div>
+
+                    </form>
                 </div>
-                <form >
-                    <div>
-                        <Input_props nombre="Nombre Completo" place="Nombre y Apellido"></Input_props>
-                    </div>
-
-                    <div>
-                        <Input_props nombre="N° Empleado" place="*****"></Input_props>
-                    </div>
-
-                    <div>
-                        <Input_props nombre="E-mail" place="Correo@correo.com"></Input_props>
-                    </div>
-
-                    <div>
-                        <Input_props nombre="Contraseña" place="Introduzca su contraseña"></Input_props>
-                    </div>
-
-                    <div>
-                        <button>Crear Cuenta</button><br />
-                        <p><a>¿Ya tienes cuenta? Iniciar sesion</a></p>
-                    </div>
-
-                </form>
             </div>
         </div>
     );
